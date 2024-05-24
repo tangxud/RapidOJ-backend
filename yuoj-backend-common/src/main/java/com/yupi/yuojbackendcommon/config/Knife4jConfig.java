9cbb57772acf7ce90c832f7cda056a2f1dfb0049
tangxudong <tangxudong@shunyouedp.com>
2024-05-24 18:32:50 +0800

    fix:修复knife4j在spring boot 2.6.x版本下与Actuator冲突

---
+++ yuoj-backend-common/src/main/java/com/yupi/yuojbackendcommon/config/Knife4jConfig.java
@@ -0,0 +1,86 @@
+package com.yupi.yuojbackendcommon.config;
+
+import com.yupi.yuojbackendcommon.utils.StringUtils;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.*;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @Author tangx
+ * @Date 2024/5/19 下午12:18
+ */
+@Configuration
+public class Knife4jConfig implements WebMvcConfigurer {
+
+    /**
+     * 解决springboot升级到2.6.x之后，knife4j报错
+     *
+     * @param webEndpointsSupplier        the web endpoints supplier
+     * @param servletEndpointsSupplier    the servlet endpoints supplier
+     * @param controllerEndpointsSupplier the controller endpoints supplier
+     * @param endpointMediaTypes          the endpoint media types
+     * @param corsProperties              the cors properties
+     * @param webEndpointProperties       the web endpoint properties
+     * @param environment                 the environment
+     * @return the web mvc endpoint handler mapping
+     */
+    @Bean
+    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(
+            WebEndpointsSupplier webEndpointsSupplier, ServletEndpointsSupplier servletEndpointsSupplier,
+            ControllerEndpointsSupplier controllerEndpointsSupplier, EndpointMediaTypes endpointMediaTypes,
+            CorsEndpointProperties corsProperties, WebEndpointProperties webEndpointProperties,
+            Environment environment) {
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
+        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+        String basePath = webEndpointProperties.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = shouldRegisterLinksMapping(webEndpointProperties,
+                environment, basePath);
+        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes,
+                corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath),
+                shouldRegisterLinksMapping, null);
+    }
+
+    /**
+     * shouldRegisterLinksMapping
+     *
+     * @param webEndpointProperties webEndpointProperties
+     * @param environment           environment
+     * @param basePath              /
+     * @return boolean
+     */
+    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties,
+                                               Environment environment, String basePath) {
+        return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath)
+                || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.yupi"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
